0e8258751cf84b4325e7f896b81f1c7d4930a7a6
tus7520024 <7520024@ed.tus.ac.jp>
2024-04-25 17:27:59 +0900

    5loop

--- loop_learning.sh
+++ loop_learning.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # loop
-while true
+for i in {1..5}
 do
+    echo "Running iteration $i..."
+    # Pythonスクリプトを実行do
     # run python and additional arguments
     python DoorHook_PPO.py num_envs=512
 
     # sleep 5
 done
+
+# chmod +x run_script.sh 必要
